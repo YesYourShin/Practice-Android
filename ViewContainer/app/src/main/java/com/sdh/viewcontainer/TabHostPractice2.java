4fdee2a1add41e92c796de0d3be318394d5b9de6
YesYourShin <ehrl827@naver.com>
2021-10-17 23:20:17 +0900

    fix

--- ViewContainer/app/src/main/java/com/sdh/viewcontainer/TabHostPractice2.java
+++ ViewContainer/app/src/main/java/com/sdh/viewcontainer/TabHostPractice2.java
@@ -6,31 +6,31 @@ import android.app.TabActivity;
 import android.os.Bundle;
 import android.widget.TabHost;
 
-@SuppressWarnings("deprecaion")
+@SuppressWarnings("deprecation")
 public class TabHostPractice2 extends TabActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tab_host_practice);
+        setContentView(R.layout.activity_tab_host_practice2);
 
         TabHost tabHost = getTabHost();
-
-        TabHost.TabSpec tabSpecCat = tabHost.newTabSpec("CAT").setIndicator("고양이");
-        tabSpecCat.setContent(R.id.tabCat);
-        tabHost.addTab(tabSpecCat);
 
         TabHost.TabSpec tabSpecDog = tabHost.newTabSpec("DOG").setIndicator("개");
         tabSpecDog.setContent(R.id.tabDog);
         tabHost.addTab(tabSpecDog);
 
-        TabHost.TabSpec tabSpecHorse = tabHost.newTabSpec("HORSE").setIndicator("우마");
-        tabSpecHorse.setContent(R.id.tabHorse);
-        tabHost.addTab(tabSpecHorse);
+        TabHost.TabSpec tabSpecCat = tabHost.newTabSpec("CAT").setIndicator("고양이");
+        tabSpecCat.setContent(R.id.tabCat);
+        tabHost.addTab(tabSpecCat);
 
         TabHost.TabSpec tabSpecRabbit = tabHost.newTabSpec("RABBIT").setIndicator("토끼");
         tabSpecRabbit.setContent(R.id.tabRabbit);
         tabHost.addTab(tabSpecRabbit);
+
+        TabHost.TabSpec tabSpecHorse = tabHost.newTabSpec("HORSE").setIndicator("말");
+        tabSpecHorse.setContent(R.id.tabHorse);
+        tabHost.addTab(tabSpecHorse);
 
         tabHost.setCurrentTab(0);
 
