526de1f0e1ad237aa09e9407e2136dd2f52604a6
YesYourShin <ehrl827@naver.com>
2021-10-21 13:52:58 +0900

    no use xml, use java

--- MenuPractice/app/src/main/java/com/sdh/menupractice/MainActivity.java
+++ MenuPractice/app/src/main/java/com/sdh/menupractice/MainActivity.java
@@ -36,32 +36,40 @@ public class MainActivity extends Activity {
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
 
-        MenuInflater mInflater = getMenuInflater();
-        if(v == button1) {
-            menu.setHeaderTitle("배경색 변경");
-            mInflater.inflate(R.menu.menu1, menu);
-        }
-        if(v == button2) {
-            mInflater.inflate(R.menu.menu2, menu);
-        }
+//        MenuInflater mInflater = getMenuInflater();
+//        if(v == button1) {
+//            menu.setHeaderTitle("배경색 변경");
+//            mInflater.inflate(R.menu.menu1, menu);
+//        }
+//        if(v == button2) {
+//            mInflater.inflate(R.menu.menu2, menu);
+//        }
+
+        menu.add(0, 1, 0, "배경색(빨강)");
+        menu.add(0, 2, 0, "배경색(초록)");
+        menu.add(0, 3, 0, "배경색(파랑)");
+
+        SubMenu sMenu = menu.addSubMenu("버튼 변경 >>");
+        sMenu.add(0, 4, 0, "버튼 45도 회전");
+        sMenu.add(0, 5, 0, "버튼 2배 확대");
     }
 
     @Override
     public boolean onContextItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.itemRed:
+            case 1:
                 baseLayout.setBackgroundColor(Color.RED);
                 return true;
-            case R.id.itemGreen:
+            case 2:
                 baseLayout.setBackgroundColor(Color.GREEN);
                 return true;
-            case R.id.itemBlue:
+            case 3:
                 baseLayout.setBackgroundColor(Color.BLUE);
                 return true;
-            case R.id.subRotate:
+            case 4:
                 button2.setRotation(45);
                 return true;
-            case R.id.subSize:
+            case 5:
                 button2.setScaleX(2);
                 return true;
         }
