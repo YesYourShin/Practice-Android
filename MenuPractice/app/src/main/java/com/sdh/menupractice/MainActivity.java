d3e084dc6b10a509a64f2e91462fe8ac2b722f18
YesYourShin <ehrl827@naver.com>
2021-10-21 13:48:47 +0900

    menu context

--- MenuPractice/app/src/main/java/com/sdh/menupractice/MainActivity.java
+++ MenuPractice/app/src/main/java/com/sdh/menupractice/MainActivity.java
@@ -3,17 +3,21 @@ package com.sdh.menupractice;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.SubMenu;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     LinearLayout baseLayout;
-    Button button1;
+    Button button1, button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,18 +27,27 @@ public class MainActivity extends AppCompatActivity {
         setTitle("배경색 바꾸기");
         baseLayout = (LinearLayout) findViewById(R.id.baseLayout);
         button1 = (Button) findViewById(R.id.button1);
+        registerForContextMenu(button1);
+        button2 = (Button) findViewById(R.id.button2);
+        registerForContextMenu(button2);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
         MenuInflater mInflater = getMenuInflater();
-        mInflater.inflate(R.menu.menu1, menu);
-        return true;
+        if(v == button1) {
+            menu.setHeaderTitle("배경색 변경");
+            mInflater.inflate(R.menu.menu1, menu);
+        }
+        if(v == button2) {
+            mInflater.inflate(R.menu.menu2, menu);
+        }
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.itemRed:
                 baseLayout.setBackgroundColor(Color.RED);
@@ -46,12 +59,49 @@ public class MainActivity extends AppCompatActivity {
                 baseLayout.setBackgroundColor(Color.BLUE);
                 return true;
             case R.id.subRotate:
-                button1.setRotation(45);
+                button2.setRotation(45);
                 return true;
             case R.id.subSize:
-                button1.setScaleX(2);
+                button2.setScaleX(2);
                 return true;
         }
         return false;
     }
+
+    //    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        super.onCreateOptionsMenu(menu);
+//
+//        menu.add(0, 1, 0, "배경색(빨강)");
+//        menu.add(0, 2, 0, "배경색(초록)");
+//        menu.add(0, 3, 0, "배경색(파랑)");
+//
+//        SubMenu sMenu = menu.addSubMenu("버튼 변경 >>");
+//        sMenu.add(0, 4, 0, "버튼 45도 회전");
+//        sMenu.add(0, 5, 0, "버튼 2배 확대");
+//
+//        return true;
+//    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case 1:
+//                baseLayout.setBackgroundColor(Color.RED);
+//                return true;
+//            case 2:
+//                baseLayout.setBackgroundColor(Color.GREEN);
+//                return true;
+//            case 3:
+//                baseLayout.setBackgroundColor(Color.BLUE);
+//                return true;
+//            case 4:
+//                button2.setRotation(45);
+//                return true;
+//            case 5:
+//                button2.setScaleX(2);
+//                return true;
+//        }
+//        return false;
+//    }
 }
