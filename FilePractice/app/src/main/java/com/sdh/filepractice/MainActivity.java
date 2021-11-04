d0d412830e93328de68d905685c9c433136c9ba4
YesYourShin <ehrl827@naver.com>
2021-11-04 12:52:29 +0900

    nikki

--- FilePractice/app/src/main/java/com/sdh/filepractice/MainActivity.java
+++ FilePractice/app/src/main/java/com/sdh/filepractice/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -26,12 +27,28 @@ public class MainActivity extends AppCompatActivity {
            public void onClick(View v) {
                try {
                    FileOutputStream outFs = openFileOutput("file.txt", Context.MODE_PRIVATE);
-                   String str = "쿡북 안드로이드";
+                   String str = "신동훈";
                    outFs.write(str.getBytes());
                    outFs.close();
                    Toast.makeText(getApplicationContext(), "file.txt가 생성됨", Toast.LENGTH_SHORT).show();
                } catch (IOException e) {}
            }
+        });
+
+        btnRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    FileInputStream inFs = openFileInput("file.txt");
+                    byte[] txt = new byte[30];
+                    inFs.read(txt);
+                    String str = new String(txt);
+                    Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT).show();
+                    inFs.close();
+                } catch (IOException e) {
+                    Toast.makeText(getApplicationContext(), "파일 없음", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
