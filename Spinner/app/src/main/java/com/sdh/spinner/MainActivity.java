e7051d38a478c739f073bfc0d29c3946b6420249
YesYourShin <ehrl827@naver.com>
2021-11-25 12:55:13 +0900

    SQLite

---
+++ Spinner/app/src/main/java/com/sdh/spinner/MainActivity.java
@@ -0,0 +1,59 @@
+package com.sdh.spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("스피너 테스트");
+
+        final String[] movie = { "토이스토리4", "호빗3", "제이슨 본", "반지의 제왕 3","정직한 후보",
+                "나쁜 녀석들", "겨울왕국 2", "알라딘", "극한직업", "스파이더맨" };
+
+        final Integer[] posterID = { R.drawable.mov01, R.drawable.mov02,
+                R.drawable.mov03, R.drawable.mov04, R.drawable.mov05,
+                R.drawable.mov06, R.drawable.mov07, R.drawable.mov08,
+                R.drawable.mov09, R.drawable.mov10 };
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner1);
+
+        ArrayAdapter<String> adapter;
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, movie);
+        spinner.setAdapter(adapter);
+
+
+        //★ 입력하기 , spinner.setOnItemSelectedListener() 사용
+        // 리스너 내부 코드
+       /*      ImageView ivPoster = (ImageView) findViewById(R.id.ivPoster);
+                ivPoster.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                ivPoster.setPadding(5, 5, 5, 5);
+                ivPoster.setImageResource(posterID[i]);
+        */
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                ImageView ivPoster = (ImageView) findViewById(R.id.ivPoster);
+                ivPoster.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                ivPoster.setPadding(5, 5, 5, 5);
+                ivPoster.setImageResource(posterID[i]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+    }
+}
